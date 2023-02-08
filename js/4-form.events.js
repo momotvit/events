df74eff0bc44e019c22eea690890d34ec8e11842
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-08 00:51:28 -0800

    form events

--- js/4-form.events.js
+++ js/4-form.events.js
@@ -1,17 +1,85 @@
-const form = document.querySelector(".form");
+// const form = document.querySelector(".form");
 
-form.addEventListener("submit", handleSubmit);
+// form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const {
-    elements: { login, password }
-  } = event.currentTarget;
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password }
+//   } = event.currentTarget;
 
-  if (login.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
+//   if (login.value === "" || password.value === "") {
+//     return console.log("Please fill in all the fields!");
+//   }
 
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+
+
+                          ///  select  ///
+
+
+
+// const select = document.querySelector(".pizza-select");
+// const textOutput = document.querySelector(".text-output");
+// const valueOutput = document.querySelector(".value-output");
+// console.log(valueOutput);
+
+// setOutput();
+
+// select.addEventListener("change", setOutput);
+
+// function setOutput() {
+//   const selectedOptionValue = select.value;
+//   const selectedOptionIndex = select.selectedIndex;
+//   const selectedOptionText = select.options[selectedOptionIndex].text;
+
+//   textOutput.textContent = selectedOptionText;
+//   valueOutput.textContent = selectedOptionValue;
+// }
+
+
+// const selectElement = document.querySelector('.ice-cream');
+
+// selectElement.addEventListener('change', (event) => {
+//   const result = document.querySelector('.result');
+//   result.textContent = `You like ${event.target.value}`;
+// });
+
+
+
+                    /////////input///////
+
+// const textInput = document.querySelector(".text-input");
+// const output = document.querySelector(".output");
+
+// textInput.addEventListener("input", (event) => {
+//   output.textContent = event.currentTarget.value;
+// });
+
+
+
+                   /////////focus and blur////////
+
+
+const textInput = document.querySelector(".text-input");
+const setFocusBtn = document.querySelector('[data-action="set"]');
+const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+setFocusBtn.addEventListener("click", () => {
+  textInput.focus();
+});
+
+removeFocusBtn.addEventListener("click", () => {
+  textInput.blur();
+});
+
+textInput.addEventListener("focus", () => {
+  textInput.value = "This input has focus";
+});
+
+textInput.addEventListener("blur", () => {
+  textInput.value = "Focus is gone! Now its blur!";
+});
