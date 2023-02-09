066b7a74261c1ab651463ef733d2016da5565b9b
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-08 20:52:12 -0800

    added one case// nri

--- js/1-events.js
+++ js/1-events.js
@@ -45,24 +45,24 @@ multiBtn.addEventListener("click", thirdCallback);
 
 //////second case removeEventListener
 
-const addListenerBtn = document.querySelector('[data-action="add"]');
-const removeListenerBtn = document.querySelector('[data-action="remove"]');
-const btn = document.querySelector("#btn");
+// const addListenerBtn = document.querySelector('[data-action="add"]');
+// const removeListenerBtn = document.querySelector('[data-action="remove"]');
+// const btn = document.querySelector("#btn");
 
-const handleClick = () => {
-  console.log("click event listener callback");
-};
+// const handleClick = () => {
+//   console.log("click event listener callback");
+// };
 
 
-addListenerBtn.addEventListener("click", () => {
-  btn.addEventListener("click", handleClick);
-  console.log("click event listener was added to btn");
-});
+// addListenerBtn.addEventListener("click", () => {
+//   btn.addEventListener("click", handleClick);
+//   console.log("click event listener was added to btn");
+// });
 
-removeListenerBtn.addEventListener("click", () => {
-  btn.removeEventListener("click", handleClick);
-  console.log("click event listener was removed from btn");
-});
+// removeListenerBtn.addEventListener("click", () => {
+//   btn.removeEventListener("click", handleClick);
+//   console.log("click event listener was removed from btn");
+// });
 
 
 
@@ -86,3 +86,38 @@ btn.addEventListener("click", mango.showUsername); // не работает
 
 // ✅ Не забывайте привязывать контекст методов объекта
 thisBtn.addEventListener("click", mango.showUsername.bind(mango));
+
+
+/*
+ * События.
+ * - Создание и удаление слушателей
+ * - Именование колбеков для слушателей
+ *    - handle*: handleSubjectEvent
+ *    - *Handler: subjectEventHandler
+ *    - on*: onSubjectEvent
+ * - Ссылочная идентичность колбеков
+ * - Объект события
+ */
+
+const targetBtn = document.querySelector('.js-target-btn');
+const addListenerBtn = document.querySelector('.js-add-listener');
+const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+addListenerBtn.addEventListener('click', event => {
+  console.log(event);
+  console.log('Вешаю слушателя события на целевую кнопку');
+
+  targetBtn.addEventListener('click', onTargetBtnClick);
+});
+
+removeListenerBtn.addEventListener('click', event => {
+  console.log(event);
+  console.log('Снимаю слушателя события с целевой кнопки');
+
+  targetBtn.removeEventListener('click', onTargetBtnClick);
+});
+
+function onTargetBtnClick(event) {
+  console.log(event);
+  console.log('Клик по целевой кнопке');
+}
